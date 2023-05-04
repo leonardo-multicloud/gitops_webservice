6b660228746ad6628e44170b4e9b66578e82fbe1
Leonardo Freitas <leonardo.freitas@ucloud.services>
2023-05-04 19:15:10 -0300

    change main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Webservice API - ArgoCD Kubernetes"))
+		w.Write([]byte("Webservice API - ArgoCD K8s"))
 	})
 	http.ListenAndServe(":8081", nil)
 }
